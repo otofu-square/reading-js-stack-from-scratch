2c8194810f19169e16155fe8326fb6fd2f8aebe6
otofu-square <yukita@feedforce.jp>
2017-04-12 00:01:25 +0900

    :+1: Add hello-button, message containers

---
+++ src/client/containers/hello-button.js
@@ -0,0 +1,16 @@
+// @flow
+
+import { connect } from 'react-redux'
+
+import { sayHello } from '../actions/hello'
+import Button from '../components/button'
+
+const mapStateToProps = () => ({
+  label: 'Say hello',
+})
+
+const mapDispatcherToProps = dispatch => ({
+  handleClick: () => { dispatch(sayHello('Hello!')) },
+})
+
+export default connect(mapStateToProps, mapDispatcherToProps)(Button)
