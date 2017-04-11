39a2a8359b37e9ae646a2e74b27766d93aab23f3
otofu-square <yukita@feedforce.jp>
2017-04-12 00:13:25 +0900

    :tada: Implement hello button

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,7 +1,15 @@
 // @flow
 
 import React from 'react'
+import HelloButton from './containers/hello-button'
+import Message from './containers/message'
+import { APP_NAME } from '../shared/config'
 
-const App = () => <h1>Hello React!!</h1>
+const App = () =>
+  <div>
+    <h1>{APP_NAME}</h1>
+    <Message />
+    <HelloButton />
+  </div>
 
 export default App
