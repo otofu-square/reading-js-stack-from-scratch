035312e0219f6ed5f7eff7f6fcc5d1d0c0d70ad4
otofu-square <yukita@feedforce.jp>
2017-05-02 17:06:00 +0900

    :tada: Enable server-side-rendering

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,6 +4,7 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Immutable from 'immutable'
 import { BrowserRouter } from 'react-router-dom'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
@@ -15,11 +16,14 @@ import helloReucer from '../shared/reducers/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 
-// eslint-disable-next-line no-underscore-dangle
+/* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const preloadedState = window.__PRELOADED_STATE__
+/* eslint-enable no-underscore-dangle */
 
 const store = createStore(
   combineReducers({ hello: helloReucer }),
+  { hello: Immutable.fromJS(preloadedState.hello) },
   composeEnhancers(applyMiddleware(thunkMiddleware)),
 )
 
@@ -39,9 +43,9 @@ ReactDOM.render(wrapApp(App, store), rootEl)
 
 if (module.hot) {
   // flow-disable-next-line
-  module.hot.accept('./app', () => {
+  module.hot.accept('../shared/app', () => {
     // eslint-disable-next-line global-require
-    const NextApp = require('./app').default
+    const NextApp = require('../shared/app').default
     ReactDOM.render(wrapApp(NextApp), rootEl)
   })
 }
